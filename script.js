82893467fce4c1b7639bd42ac97747c11c0b9bcb
PotembergMarieManuela <potemberg.mariemanuela@hotmail.com>
2020-08-16 19:31:46 +0100

    IOLCE CaseStudy

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const button = document.querySelector('.button--nav');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('.body');
+
+button.onclick = function()
+{
+    nav.classList.toggle('nav--open');
+}
+
+body.onclick = function()
+{
+    nav.classList.remove('nav--open');
+}
